286f61e01f2eaadcd194aa9e336d0092c09787d3
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-10-24 01:13:02 +0600

    verifyadmin and check admin api create

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ app.use(express.json());
 
 const verifyJwt = (req, res, next) => {
   const authorization = req.headers.authorization;
-  if(!authorization){
-    res.status(401).send({error: true, message: 'unauthorize access'});
+  if (!authorization) {
+    res.status(401).send({ error: true, message: "unauthorize access" });
   }
-  
-  const token = authorization.split(' ')[1];
+
+  const token = authorization.split(" ")[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
-    if(error){
-      res.status(401).send({error: true, message: 'unauthorize access'});
+    if (error) {
+      res.status(401).send({ error: true, message: "unauthorize access" });
     }
     req.decoded = decoded;
     next();
-  })
-}
+  });
+};
 
 const uri = `mongodb+srv://${process.env.USERDB_NAME}:${process.env.USERDB_PASS}@cluster0.5a8lj4m.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -47,9 +47,17 @@ async function run() {
     const serviceCartCollection = client.db("hairCutting").collection("cart");
     const userCollection = client.db("hairCutting").collection("user");
 
-    // const verifyAdmin = (res, req, next) => {
-    //   const filter = ()
-    // }
+    const verifyAdmin = async (res, req, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "unauthorized access" });
+      }
+      next();
+    };
 
     //jwt
     app.post("/jwt", (req, res) => {
@@ -61,45 +69,57 @@ async function run() {
     });
 
     //user api
-    app.post('/user', async (req, res) => {
-        const user = req.body;
-        console.log(user);
-        const query = {email : user.email}
-        const existingUser = await userCollection.findOne(query);
-        console.log(existingUser);
-        if(existingUser){
-          return res.send({message: 'User already exist.'})
-        }
-        const result = await userCollection.insertOne(user);
-       res.send(result);
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      console.log(existingUser);
+      if (existingUser) {
+        return res.send({ message: "User already exist." });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
     });
 
     //get all user
-    app.get('/user', async(req, res) => {
+    app.get("/user", verifyJwt,  async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-    })
-    
+    });
+
     //make admin role
-    app.patch('/admin/user/:id', async(req, res) => {
+    app.patch("/admin/user/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id) }
-      const updateDoc ={
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
         $set: {
-          role : "admin"
-        }
-      }
+          role: "admin",
+        },
+      };
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
-    })
+    });
+
+    //check admin
+    app.get("/user/admin/:email", verifyJwt, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
 
     //user delete
-    app.delete('/user/:id', async (req, res) => {
+    app.delete("/user/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(filter);
       res.send(result);
-    })
+    });
 
     //services api
     app.get("/service", async (req, res) => {
@@ -108,12 +128,12 @@ async function run() {
     });
 
     //service deleted
-    app.delete('/service/:id', async (req, res) => {
+    app.delete("/service/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const result = await serviceCollection.deleteOne(filter);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     //review collection
     app.get("/review", async (req, res) => {
@@ -122,38 +142,34 @@ async function run() {
     });
 
     //review add
-    app.post('/addreview', async(req, res) => {
+    app.post("/addreview", async (req, res) => {
       const newReview = req.body;
       console.log(newReview);
       const result = await reviewCollection.insertOne(newReview);
       res.send(result);
-    })
+    });
 
     //service added in cart
-    app.post('/cart', async (req, res) => {
+    app.post("/cart", async (req, res) => {
       const service = req.body;
       const result = await serviceCartCollection.insertOne(service);
       res.send(result);
-    })
-
-    
+    });
 
     //service order list show in order list
-    app.get('/cart', async(req, res) => {
+    app.get("/orderlist", async (req, res) => {
       const result = await serviceCartCollection.find().toArray();
       res.send(result);
-    })
+    });
 
     //cart get by email
-    app.get('/cart', async (req, res) => {
-      const email = req.params.email;
+    app.get("/cart", verifyJwt, async (req, res) => {
+      const email = req.query.email;
       console.log(email);
-      const query = { email: new ObjectId(email) };
+      const query = { email: email };
       const result = await serviceCartCollection.find(query).toArray();
       res.send(result);
-    })
-
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
